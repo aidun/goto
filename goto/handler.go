0852f2579326adb812dc72b87866b270b0c8fb29
Markus Hartmann <markush1986@gmail.com>
2020-03-06 20:21:05 +0100

    Update handler.go

--- goto/handler.go
+++ goto/handler.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// Handle a serverless requestdfddf
+// Handle a serverless requestdfdasdfdf
 func Handle(req []byte) string {
 	return fmt.Sprintf("Hedlldfo, Gdo. You said: %s", string(req))
 }
